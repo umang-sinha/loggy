5756942a6648069e196840660b9d19a4a25df98f
umang-sinha <umangsinha12@gmail.com>
2025-03-11 08:10:55 +0530

    chore(client): :coffin: clean up

--- client/tests/LoggyLogger.test.ts
+++
@@ -1,7 +0,0 @@
-import { LoggyLogger } from "../src/LoggyLogger";
-
-describe("LoggyLogger", () => {
-  it("should log a messgae", () => {
-    LoggyLogger.log("INFO", "Test log message", { key: "value" });
-  });
-});
